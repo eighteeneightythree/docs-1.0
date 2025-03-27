2bce507e152cde90d1bde5485668dc57522314ca
eighteeneightythree <louisnel.me@gmail.com>
2025-03-27 22:01:37 +0200

    Update fonts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,9 +24,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Open sans",
+        body: "Open Sans",
+        code: "Source Code Pro",
       },
       colors: {
         lightMode: {

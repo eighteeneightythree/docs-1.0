83c03e9d3c7c0b36c6018f99ea1f11a9ecf1219f
eighteeneightythree <48271257+eighteeneightythree@users.noreply.github.com>
2025-11-18 22:58:56 +1300

    Update config

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "docs.louisnel.co.nz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
